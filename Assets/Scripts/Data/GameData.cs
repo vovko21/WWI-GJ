7050ae0887ab58466dbcf2de875098ca3a611edb
vovko21 <vovako98@gmail.com>
2024-04-20 18:07:01 +0300

    fixed generator

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 public class GameData : SingletonMonobehaviour<GameData>
 {
     private List<PersonData> _personsData;
+    private List<PersonData> _impostersData;
 
     public List<PersonData> PersonsData => _personsData;
+    public List<PersonData> ImpostersData => _impostersData;
 
-    public void Initialize(List<PersonData> personsData)
+    public void Initialize(List<PersonData> personsData, List<PersonData> impostersData)
     {
         _personsData = personsData;
+        _impostersData = impostersData;
     }
 }
--- Assets/Scripts/Generator/DataGenerator.cs
+++ Assets/Scripts/Generator/DataGenerator.cs
@@ -44,4 +44,9 @@ public class DataGenerator
 
         return person;
     }
+
+    public void GetPerson()
+    {
+
+    }
 }
